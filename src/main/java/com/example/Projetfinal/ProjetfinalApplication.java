c1e59a19f7f30b89d557d4917bcec74c887fd558
ahmed <khemilihmd@gmail.com>
2025-03-10 10:55:56 +0100

    first commit

---
+++ src/main/java/com/example/Projetfinal/ProjetfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projetfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projetfinal/ProjetfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projetfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
